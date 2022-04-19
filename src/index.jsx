5f3c2a8f89a435d6a42a616530b5fe49e0055b7a
Maryam Verdon <maryamverdon@gmail.com>
2022-04-19 16:12:38 +0200

    ajout de SurveyProvider

--- src/index.jsx
+++ src/index.jsx
@@ -3,41 +3,41 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
-import Header from './components/Header'
-import Error from './components/Error'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
-import GlobalStyle from './utils/style/GlobalStyle'
+import Header from './components/Header'
 import Footer from './components/Footer'
-import { ThemeProvider } from './utils/context'
-
-
+import Error from './components/Error'
+import GlobalStyle from './utils/style/GlobalStyle'
+import { ThemeProvider, SurveyProvider } from './utils/context'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <ThemeProvider>
-      <GlobalStyle /> 
-        <Header /> 
-        <Switch>
-          <Route exact path="/">
+        <SurveyProvider>
+          <GlobalStyle />
+          <Header />
+          <Switch>
+            <Route exact path="/">
               <Home />
-          </Route>
-          <Route path="/survey/:questionNumber">
-            <Survey />
-          </Route>
-          <Route path="/results">
-            <Results />
-          </Route>
-          <Route path="/freelances">
-            <Freelances />
-          </Route>
-          <Route>
-            <Error />
-          </Route>
-        </Switch>
-        <Footer />
-      </ThemeProvider> 
+            </Route>
+            <Route path="/survey/:questionNumber">
+              <Survey />
+            </Route>
+            <Route path="/results">
+              <Results />
+            </Route>
+            <Route path="/freelances">
+              <Freelances />
+            </Route>
+            <Route>
+              <Error />
+            </Route>
+          </Switch>
+          <Footer />
+        </SurveyProvider>
+      </ThemeProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
